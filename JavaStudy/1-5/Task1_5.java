657d4503001d88dbf8a75ae563cce30a869804bb
Ituking <77528963+Ituking@users.noreply.github.com>
2022-07-13 23:08:43 +0900

    課題提出テスト
    1-5の作成

--- JavaStudy/1-5/Task1_5.java
+++ JavaStudy/1-5/Task1_5.java
@@ -46,15 +46,13 @@ public class Task1_5 {
         //         System.out.print(x + "||" + x + "×" + k + "=" + (x * k));
         //     }        
         // }
-        for (int z = 1; z <= 9; z++) {
-            System.out.println(z + " || ");
-        }
+        // for (int z = 1; z <= 9; z++) {
+        //     System.out.println(z + " || ");
+        // }
         for (int x = 1; x <= 9; x++) {
-            for (int k = 1; k <= 9; k++) {
-                int z = (x * k);
-                System.out.print(z + "|");
-            }        
+            for (int y = 1; y <= 9; y++) {
+                System.out.print(" " + x * y);
+            }System.out.println();
         }
-        System.out.println();
     }
 }
