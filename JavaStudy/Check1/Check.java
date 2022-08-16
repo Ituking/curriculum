0af47e3ca02907fde026a1974e5802a0abd6c1e0
Ituking <77528963+Ituking@users.noreply.github.com>
2022-08-16 22:53:07 +0900

    課題提出テスト
    Check1の作成

--- JavaStudy/Check1/Check.java
+++ JavaStudy/Check1/Check.java
@@ -20,12 +20,12 @@ public class Check{
         }
         System.out.println();
         for (int i = 0; i < data.length; i++) {
-            for (int j = data.length; j > i; j--) {
+            for (int j = data.length - 1; j > i; j--) {
                 /*
                 * 問3
                 * 以下、配列の添字を入れてソートを完成させなさい
                 */
-                if(data[i] > data[j]){
+                if(data[j] > data[i]){
                   int box = data[i];
                   data[i] = data[j];
                   data[j] = box;
